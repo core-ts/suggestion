d5ece1d5c341d0588b8e6b30f5076ba3e791bfe3
vinhtq2020 <quangvinh2015vi@gmail.com>
2022-05-06 11:36:56 +0700

    update suggestion

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,43 @@
-export interface DataLoader<T> {
-  loadData(keyWords: string, max: number, ctx?: any): Promise<T[]>;
+export interface PreviousData<T> {
+  keyword: string;
+  list: T[];
 }
 
-export interface SuggestionService<T> {
-  getSuggestion(keyword: string, previousSuggestion: PreviousSuggestion<T>, ctx?: any): Promise<Suggestion<T>>;
+export interface Suggestion<T> {
+  list: T[];
+  last: PreviousData<T>;
 }
 
-export class PreviousSuggestion<T> {
-  previousKeyWord: string;
-  result: T[];
-}
-
-export class Suggestion<T> {
-  response: T[];
-  previousSuggestion: PreviousSuggestion<T>;
-}
-
-export class DefaultSuggestionService<T> implements SuggestionService<T> {
-  constructor(
-      private service: DataLoader<T>,
-      private max: number,
-      private displayField: string = '',
-      private valueField: string = '') {
-    this.getSuggestion = this.getSuggestion.bind(this);
+export class SuggestionService<T> {
+  constructor(protected loadData: (keyword: string, max: number, ctx?: any) => Promise<T[]>, public max: number = 20, public displayField: string = '', public valueField: string = '') {
+    this.load = this.load.bind(this);
   }
-
-  async getSuggestion(keyword: string, previousSuggestion: PreviousSuggestion<T>, excludingValues: any[] = [], ctx?: any): Promise<Suggestion<T>> {
+  load(keyword: string, last: PreviousData<T>, excludingValues?: T[]): Promise<Suggestion<T>> {
     if (
-      keyword.length > 1 && keyword.startsWith(previousSuggestion.previousKeyWord) &&
-      previousSuggestion.result.length < this.max
+      keyword.length > 1 && keyword.startsWith(last.keyword) &&
+      last.list.length < this.max
     ) {
-      let response;
+      let list: T[] | undefined;
       keyword = keyword.toUpperCase();
       if (this.displayField !== '') {
-        response = previousSuggestion.result.filter(item => item[this.displayField].toUpperCase().includes(keyword));
+        list = last.list.filter(item => (item as any)[this.displayField].toUpperCase().includes(keyword));
       } else {
-        response = previousSuggestion.result.filter(item => String(item).toUpperCase().includes(keyword));
+        list = last.list.filter(item => String(item).toUpperCase().includes(keyword));
       }
-      return Promise.resolve({ response, previousSuggestion });
+      return Promise.resolve({ list: list, last });
     } else {
-      let response = await this.service.loadData(keyword, this.max, ctx);
-      if (excludingValues.length > 0) {
-        if (this.valueField !== '') {
-          response = response.filter(obj => !excludingValues.find(item => obj[this.valueField] === item));
-        } else {
-          response = response.filter(obj => !excludingValues.find(item => obj === item));
+      return this.loadData(keyword, this.max).then(list => {
+        if (excludingValues && excludingValues.length > 0) {
+          if (this.valueField !== '') {
+            list = list.filter(obj => !excludingValues.find(item => (obj as any)[this.valueField] === item));
+          } else {
+            list = list.filter(obj => !excludingValues.find(item => obj === item));
+          }
         }
-      }
-      previousSuggestion.previousKeyWord = keyword;
-      previousSuggestion.result = response;
-      return { response, previousSuggestion };
+        last.keyword = keyword;
+        last.list = list;
+        return { list, last };
+      })
     }
   }
 }
